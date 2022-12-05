4a3ceab9c7875d33bd413ca006b6574905aa401b
Emma Johnson <emma58@users.noreply.github.com>
2022-12-04 21:31:03 -0700

    Linking the original binary variables to the indicator variables instead of adding constraints on the binaries on the Disjuncts, making a uniquely named transformation block, removing a lot of unused objects on the transformation block, indexing the Disjuncts and Disjunctions by Any

--- pyomo/gdp/plugins/bilinear.py
+++ pyomo/gdp/plugins/bilinear.py
@@ -11,9 +11,10 @@
 
 import logging
 
+from pyomo.common.modeling import unique_component_name
 from pyomo.core import (
     TransformationFactory, Transformation, Block, VarList, Set,
-    SortComponents, Objective, Constraint)
+    SortComponents, Objective, Constraint, ConstraintList, Any)
 from pyomo.gdp import Disjunct, Disjunction
 from pyomo.repn import generate_standard_repn
 
@@ -25,61 +26,70 @@ logger = logging.getLogger('pyomo.gdp')
     doc="Creates a disjunctive model where bilinear terms are replaced with "
     "disjunctive expressions.")
 class Bilinear_Transformation(Transformation):
-
+    """
+    TODO: 
+    * write a docstring
+    * allow targets (so that you can do this inside Disjuncts too)
+    """
     def __init__(self):
         super(Bilinear_Transformation, self).__init__()
+        self._transformation_blocks = {}
 
     def _apply_to(self, instance, **kwds):
-        # TODO: This data should be stored differently.  We cannot nest this
-        # transformation with itself
-        if getattr(instance, 'bilinear_data_', None) is None:
-            instance.bilinear_data_ = Block()
-            instance.bilinear_data_.cache = {}
-            instance.bilinear_data_.vlist = VarList()
-            instance.bilinear_data_.vlist_boolean = []
-            instance.bilinear_data_.IDX = Set()
-            instance.bilinear_data_.disjuncts_ = Disjunct(
-                instance.bilinear_data_.IDX*[0,1])
-            instance.bilinear_data_.disjunction_data = {}
-            instance.bilinear_data_.o_expr = {}
-            instance.bilinear_data_.c_body = {}
+        try:
+            self._apply_to_impl(instance, **kwds)
+        finally:
+            self._transformation_blocks.clear()
+
+    def _apply_to_impl(self, instance, **kwds):
+        transBlock = self._add_transformation_block(instance)
         #
         # Iterate over all blocks
         #
         for block in instance.block_data_objects(
-                active=True, sort=SortComponents.deterministic ):
-            self._transformBlock(block, instance)
-        #
-        # WEH: I wish I had a DisjunctList and DisjunctionList object...
-        #
-        def rule(block, i):
-            return instance.bilinear_data_.disjunction_data[i]
-        instance.bilinear_data_.disjunction_ = Disjunction(
-            instance.bilinear_data_.IDX, rule=rule)
+                active=True, sort=SortComponents.deterministic):
+            self._transformBlock(block, transBlock)
 
-    def _transformBlock(self, block, instance):
+    def _add_transformation_block(self, to_block):
+        if to_block in self._transformation_blocks:
+            return self._transformation_blocks[to_block]
+
+        # make a transformation block on to_block to put transformed disjuncts
+        # on
+        transBlockName = unique_component_name(
+            to_block,
+            '_pyomo_gdp_bilinear_reformulation')
+        self._transformation_blocks[to_block] = transBlock = Block()
+        to_block.add_component(transBlockName, transBlock)
+        transBlock.cache = {}
+        transBlock.vlist = VarList()
+        transBlock.disjuncts_ = Disjunct(Any)
+        transBlock.disjunctions = Disjunction(Any)
+        transBlock.constraints = ConstraintList()
+
+        return transBlock
+
+    def _transformBlock(self, block, transBlock):
         for component in block.component_objects(Objective, active=True,
                                                  descend_into=False):
-            expr = self._transformExpression(component.expr, instance)
-            instance.bilinear_data_.o_expr[ id(component) ] = component.expr
+            expr = self._transformExpression(component.expr, transBlock)
             component.expr = expr
         for component in block.component_data_objects(Constraint, active=True,
                                                       descend_into=False):
-            expr = self._transformExpression(component.body, instance)
-            instance.bilinear_data_.c_body[ id(component) ] = component.body
+            expr = self._transformExpression(component.body, transBlock)
             component._body = expr
 
-    def _transformExpression(self, expr, instance):
+    def _transformExpression(self, expr, transBlock):
         if expr.polynomial_degree() > 2:
             raise ValueError(
                 "Cannot transform polynomial terms with degree > 2")
         if expr.polynomial_degree() < 2:
             return expr
         #
-        expr = self._replace_bilinear(expr, instance)
+        expr = self._replace_bilinear(expr, transBlock)
         return expr
 
-    def _replace_bilinear(self, expr, instance):
+    def _replace_bilinear(self, expr, transBlock):
         idMap = {}
         terms = generate_standard_repn(expr, idMap=idMap)
         # Constant
@@ -93,61 +103,54 @@ class Bilinear_Transformation(Transformation):
                                     terms.quadratic_coefs):
                 #
                 if vars_[0].is_binary():
-                    v = instance.bilinear_data_.cache.get(
-                        (id(vars_[0]),id(vars_[1])), None)
+                    v = transBlock.cache.get((id(vars_[0]), id(vars_[1])), None)
                     if v is None:
-                        instance.bilinear_data_.vlist_boolean.append(vars_[0])
-                        v = instance.bilinear_data_.vlist.add()
-                        instance.bilinear_data_.cache[id(vars_[0]),
-                                                      id(vars_[1])] = v
+                        v = transBlock.vlist.add()
+                        transBlock.cache[id(vars_[0]), id(vars_[1])] = v
                         bounds = vars_[1].bounds
                         v.setlb(bounds[0])
                         v.setub(bounds[1])
-                        id_ = len(instance.bilinear_data_.vlist)
-                        instance.bilinear_data_.IDX.add(id_)
+                        id_ = len(transBlock.vlist)
                         # First disjunct
-                        d0 = instance.bilinear_data_.disjuncts_[id_,0]
-                        d0.c1 = Constraint(expr=vars_[0] == 1)
+                        d0 = transBlock.disjuncts_[id_,0]
+                        #d0.c1 = Constraint(expr=vars_[0] == 1)
                         d0.c2 = Constraint(expr=v == vars_[1])
                         # Second disjunct
-                        d1 = instance.bilinear_data_.disjuncts_[id_,1]
-                        d1.c1 = Constraint(expr=vars_[0] == 0)
+                        d1 = transBlock.disjuncts_[id_,1]
+                        #d1.c1 = Constraint(expr=vars_[0] == 0)
                         d1.c2 = Constraint(expr=v == 0)
                         # Disjunction
-                        instance.bilinear_data_.disjunction_data[
-                            id_] = [instance.bilinear_data_.disjuncts_[id_,0],
-                                    instance.bilinear_data_.disjuncts_[id_,1]]
-                        instance.bilinear_data_.disjunction_data[
-                            id_] = [instance.bilinear_data_.disjuncts_[id_,0],
-                                    instance.bilinear_data_.disjuncts_[id_,1]]
+                        transBlock.disjunctions[id_] = [
+                            transBlock.disjuncts_[id_,0],
+                            transBlock.disjuncts_[id_,1]
+                        ]
+                        # link the original binary and the indicator var
+                        transBlock.constraints.add(
+                            vars_[0] == d0.binary_indicator_var)
                     # The disjunctive variable is the expression
                     e += coef_*v
                 #
                 elif vars_[1].is_binary():
-                    v = instance.bilinear_data_.cache.get((id(vars_[1]),
-                                                           id(vars_[0])), None)
+                    v = transBlock.cache.get((id(vars_[1]), id(vars_[0])), None)
                     if v is None:
-                        instance.bilinear_data_.vlist_boolean.append(vars_[1])
-                        v = instance.bilinear_data_.vlist.add()
-                        instance.bilinear_data_.cache[id(vars_[1]),
-                                                      id(vars_[0])] = v
+                        v = transBlock.vlist.add()
+                        transBlock.cache[id(vars_[1]), id(vars_[0])] = v
                         bounds = vars_[0].bounds
                         v.setlb(bounds[0])
                         v.setub(bounds[1])
-                        id_ = len(instance.bilinear_data_.vlist)
-                        instance.bilinear_data_.IDX.add(id_)
+                        id_ = len(transBlock.vlist)
                         # First disjunct
-                        d0 = instance.bilinear_data_.disjuncts_[id_,0]
+                        d0 = transBlock.disjuncts_[id_,0]
                         d0.c1 = Constraint(expr=vars_[1] == 1)
                         d0.c2 = Constraint(expr=v == vars_[0])
                         # Second disjunct
-                        d1 = instance.bilinear_data_.disjuncts_[id_,1]
+                        d1 = transBlock.disjuncts_[id_,1]
                         d1.c1 = Constraint(expr=vars_[1] == 0)
                         d1.c2 = Constraint(expr=v == 0)
                         # Disjunction
-                        instance.bilinear_data_.disjunction_data[
-                            id_] = [instance.bilinear_data_.disjuncts_[id_,0],
-                                    instance.bilinear_data_.disjuncts_[id_,1]]
+                        transBlock.disjunctions[id_] = [
+                            transBlock.disjuncts_[id_,0],
+                            transBlock.disjuncts_[id_,1]]
                     # The disjunctive variable is the expression
                     e += coef_*v
                 else:
