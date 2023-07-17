b7166973070da484a888c065d300f3d9828f73e0
Emma Johnson <emma58@users.noreply.github.com>
2023-07-17 17:24:57 -0600

    Making the test that we don't have a typo anymore a better test

--- pyomo/gdp/tests/test_hull.py
+++ pyomo/gdp/tests/test_hull.py
@@ -1789,6 +1789,8 @@ class NestedDisjunction(unittest.TestCase, CommonTests):
         x_cons_child = hull.get_disaggregation_constraint(m.x, m.parent1.disjunction)
         assertExpressionsEqual(self, x_cons_child.expr, x_p1 == x_c1 + x_c2 + x_c3)
 
+    #def check_convex_combo_disjunction(self, 
+
     def test_nested_with_local_vars(self):
         m = ConcreteModel()
 
@@ -1805,15 +1807,13 @@ class NestedDisjunction(unittest.TestCase, CommonTests):
 
         @m.Disjunct()
         def d_r(d):
-            d.y = Var(bounds=(0, 10))
-
             @d.Disjunct()
             def d_l(e):
                 e.lambdas = Var(m.S, bounds=(0, 1))
                 e.LocalVars = Suffix(direction=Suffix.LOCAL)
                 e.LocalVars[e] = list(e.lambdas.values())
                 e.c1 = Constraint(expr=e.lambdas[1] + e.lambdas[2] == 1)
-                e.c2 = Constraint(expr=m.x == 4 * e.lambdas[1] + 2 * e.lambdas[2])
+                e.c2 = Constraint(expr=m.x == 2 * e.lambdas[1] + 3 * e.lambdas[2])
 
             @d.Disjunct()
             def d_r(e):
@@ -1821,7 +1821,7 @@ class NestedDisjunction(unittest.TestCase, CommonTests):
                 e.LocalVars = Suffix(direction=Suffix.LOCAL)
                 e.LocalVars[e] = list(e.lambdas.values())
                 e.c1 = Constraint(expr=e.lambdas[1] + e.lambdas[2] == 1)
-                e.c2 = Constraint(expr=m.x == 7 * e.lambdas[1] + 6 * e.lambdas[2])
+                e.c2 = Constraint(expr=m.x == 2 * e.lambdas[1] + 3 * e.lambdas[2])
 
             d.inner_disj = Disjunction(expr=[d.d_l, d.d_r])
 
@@ -1830,6 +1830,40 @@ class NestedDisjunction(unittest.TestCase, CommonTests):
 
         hull = TransformationFactory('gdp.hull')
         hull.apply_to(m)
+
+        x1 = hull.get_disaggregated_var(m.x, m.d_l)
+        x2 = hull.get_disaggregated_var(m.x, m.d_r)
+        x3 = hull.get_disaggregated_var(m.x, m.d_r.d_l)
+        x4 = hull.get_disaggregated_var(m.x, m.d_r.d_r)
+
+        for d, x in [(m.d_l, x1), (m.d_r.d_l, x3), (m.d_r.d_r, x4)]:
+            lambda1 = hull.get_disaggregated_var(d.lambdas[1], d)
+            self.assertIs(lambda1, d.lambdas[1])
+            lambda2 = hull.get_disaggregated_var(d.lambdas[2], d)
+            self.assertIs(lambda2, d.lambdas[2])
+        
+            cons = hull.get_transformed_constraints(d.c1)
+            self.assertEqual(len(cons), 1)
+            convex_combo = cons[0]
+            assertExpressionsEqual(self, convex_combo.expr, lambda1 + lambda2 -
+                                   (1 -
+                                    d.indicator_var.get_associated_binary())*0.0
+                                   == d.indicator_var.get_associated_binary())
+            cons = hull.get_transformed_constraints(d.c2)
+            self.assertEqual(len(cons), 1)
+            get_x = cons[0]
+            assertExpressionsEqual(self, get_x.expr, x - (2*lambda1 + 3*lambda2)
+                                   - (1 -
+                                      d.indicator_var.get_associated_binary())* 0.0
+                                   ==
+                                   0.0*d.indicator_var.get_associated_binary())
+
+        cons = hull.get_disaggregation_constraint(m.x, m.disj)
+        assertExpressionsEqual(self, cons.expr,
+                               m.x == x2 + x1)
+        cons = hull.get_disaggregation_constraint(m.x, m.d_r.inner_disj)
+        assertExpressionsEqual(self, cons.expr,
+                               x2 == x3 + x4)
 
     def test_nested_with_var_that_skips_a_level(self):
         m = ConcreteModel()
